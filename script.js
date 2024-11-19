ca0e6813b6ade2b4a5f2e87ad12cb1ea2f95ebff
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2024-11-19 20:45:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let n=document.querySelector("#level");
+
+let count=0;
+let curr=n;
+while(curr){
+	count++;
+	curr=curr.parentElement;
+}
+alert(`The level of the element is: ${count}`) 
